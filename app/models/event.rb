3e87d5e9723f75d4f0258eefd4ce458d337aadf7
KishiKyosuke <carp011808pg@icloud.com>
2021-11-01 23:32:54 +0900

    Added image file type validation to event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,6 +5,7 @@ class Event < ApplicationRecord
   validates :start_at, presence: true
   validates :end_at, presence: true
   validate :start_at_should_be_before_end_at
+  validates :image, content_type: [:png, :jpg, :jpeg]
 
   has_one_attached :image, dependent: false
   belongs_to :owner, class_name: "User"
